6849bc976c1a750cb44fdc386b07875bfaff7c89
lourdescabezaolias@gmail.com <lourdes@MacBook-Pro-de-Lourdes.local>
2025-04-26 14:05:16 +0200

    first commit studentA

---
+++ main.py
@@ -0,0 +1,12 @@
+import utilities as utilA
+
+def main():
+    a = 4
+    b = 5
+    c = utilA.suma(a,b)
+
+    print(f"El valor de la suma de a y b es {c}")
+
+if __name__ == "__main__":
+    main()
+
